7e925f9c5313bc3d71e47a8ea37292079c9d3dc9
ArayaCG <arayag64@gmail.com>
2024-05-29 10:30:13 -0300

    Corrección DTO

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -11,7 +11,7 @@ import {
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { TaskService } from './task.service';
-import { TaskDto } from './task.dto';
+import { CreateTaskDto, UpdateTaskDto } from './task.dto';
 
 @ApiTags('Tasks')
 @ApiBearerAuth()
@@ -30,13 +30,13 @@ export class TaskController {
   }
 
   @Post()
-  async create(@Body() newTask: TaskDto) {
+  async create(@Body() newTask: CreateTaskDto) {
     const task = await this.taskService.createTask(newTask);
     return task;
   }
 
   @Put(':id')
-  async update(@Param('id') id: string, @Body() updateTask: TaskDto) {
+  async update(@Param('id') id: string, @Body() updateTask: UpdateTaskDto) {
     try {
       return await this.taskService.updateTask(id, updateTask);
     } catch (error) {
--- src/task/task.dto.ts
+++ src/task/task.dto.ts
@@ -1,12 +1,61 @@
-import { IsString, IsOptional, Length, IsInt, IsNotEmpty } from 'class-validator';
+import {
+  IsString,
+  IsOptional,
+  Length,
+  IsInt,
+  IsNotEmpty,
+} from 'class-validator';
 
-export class TaskDto {
+export class CreateTaskDto {
   /**
    * The name must be a non-empty string of maximum 50 characters.
    * @example "Example Task"
    */
   @IsString()
   @IsNotEmpty()
+  @Length(1, 50)
+  name: string;
+
+  /**
+   * The description must be a string and can be optional.
+   * @example "This is an example task."
+   */
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  /**
+   * The status must be a string and can be optional.
+   * @example "In Progress"
+   */
+  @IsOptional()
+  @IsString()
+  status: string;
+
+  /**
+   * The priority must be an integer and can be optional.
+   * @example 1
+   */
+  @IsOptional()
+  @IsInt()
+  priority: number;
+
+  /**
+   * The story points must be an integer and can be optional.
+   * @example 2
+   */
+  @IsOptional()
+  @IsInt()
+  story_points: number;
+}
+
+export class UpdateTaskDto{
+  /**
+   * The name must be a non-empty string of maximum 50 characters.
+   * @example "Example Task"
+   */
+  @IsString()
+  @IsOptional()
   @Length(1, 50)
   name: string;
 
