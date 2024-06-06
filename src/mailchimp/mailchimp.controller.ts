1a74db794d2e99709c276579d87fddf47aaf5134
alejandro0419 <alejandrocampaya@hotmail.com>
2024-06-06 13:12:44 -0300

    mailchimp and seeder docs

--- src/mailchimp/mailchimp.controller.ts
+++ src/mailchimp/mailchimp.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { MailchimpService } from './mailchimp.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('newsletter')
 export class MailchimpController {
   constructor(private readonly mailchimpService: MailchimpService) {}
 
   @Post('subscribe')
+  @ApiOperation({ summary: 'Suscribes a user to the Newsletter', 
+    description: 'Expects the email, firstName and lastName in the body of the request. Returns a success or error message.'
+   })
   async subscribe(@Body() body: { email: string; firstName: string; lastName: string }) {
     const { email, firstName, lastName } = body;
     const response = await this.mailchimpService.addSubscriber(email, firstName, lastName).toPromise();
