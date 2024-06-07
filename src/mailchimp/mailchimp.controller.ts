9fa8c3c1800caa4309d48fb6c55b53dabc90a075
alejandro0419 <alejandrocampaya@hotmail.com>
2024-06-07 12:15:47 -0300

    profilepic fix and mailchimp body fix

--- src/mailchimp/mailchimp.controller.ts
+++ src/mailchimp/mailchimp.controller.ts
@@ -1,18 +1,24 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { MailchimpService } from './mailchimp.service';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { MailchimpDto } from './mailchimp.dto';
 
+@ApiTags('Newsletter')
 @Controller('newsletter')
 export class MailchimpController {
   constructor(private readonly mailchimpService: MailchimpService) {}
 
   @Post('subscribe')
-  @ApiOperation({ summary: 'Suscribes a user to the Newsletter', 
-    description: 'Expects the email, firstName and lastName in the body of the request. Returns a success or error message.'
-   })
-  async subscribe(@Body() body: { email: string; firstName: string; lastName: string }) {
+  @ApiOperation({
+    summary: 'Suscribes a user to the Newsletter',
+    description:
+      'Expects the email, firstName and lastName in the body of the request. Returns a success or error message.'
+  })
+  async subscribe(@Body() body: MailchimpDto) {
     const { email, firstName, lastName } = body;
-    const response = await this.mailchimpService.addSubscriber(email, firstName, lastName).toPromise();
+    const response = await this.mailchimpService
+      .addSubscriber(email, firstName, lastName)
+      .toPromise();
     return response;
   }
 }
