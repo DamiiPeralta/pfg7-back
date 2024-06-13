6eeafc99d0f3f9bf5a2e23f998b12f295a93973b
ArayaCG <arayag64@gmail.com>
2024-06-13 12:54:52 -0300

    enum de task

--- src/task/task.entity.ts
+++ src/task/task.entity.ts
@@ -2,6 +2,7 @@ import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, ManyToMany, DeleteDa
 import { User } from 'src/user/user.entity';
 import { Team } from 'src/team/team.entity';
 import { Sprint } from 'src/sprint/sprint.entity';
+import { statusTask } from './task.enum';
 
 @Entity({ name: 'tasks' })
 export class Task {
@@ -44,8 +45,8 @@ export class Task {
    * The status must be a string and can be null.
    * @example "In Progress"
    */
-  @Column({ type: "varchar", nullable: true })
-  status: string;
+  @Column({ type: "enum",enum: statusTask, nullable: true })
+  status: statusTask;
 
   /**
    * The priority must be an integer and can be null.
---
+++ src/task/task.enum.ts
@@ -0,0 +1,6 @@
+export enum statusTask {
+    open = "Open",
+    inProgress = "In progress",
+    testing = "Testing",
+    done = "Done",
+}
